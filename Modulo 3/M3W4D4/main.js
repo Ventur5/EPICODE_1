1260e8d780459ee5fe726f4e30ad3234e44be10a
Valerio Ventura <valerioventura229@gmail.com>
2025-10-02 20:06:05 +0200

    Aggiornamento ESERCIZIO CON VOTO M3W4D4

--- Modulo 3/M3W4D4/main.js
+++ Modulo 3/M3W4D4/main.js
@@ -1,43 +1,22 @@
-const burgerMenuButton = document.querySelector('.burger-menu');
-const burgerIcons = document.querySelectorAll('.hamburger-icon');
-const sideBar = document.querySelector('.side-bar');
-const menu = document.querySelector('.menu');
-const navBar = document.querySelector('nav');
-const heroImg = document.querySelector('.hero-img');
+const burgerMenuButton = document.querySelector(".burger-menu");
+const hamburgerIcon = document.querySelector(".hamburger-icon"); // 👈 usa solo questo!
+const sideBar = document.querySelector(".side-bar");
+const menu = document.querySelector(".menu");
+const heroImg = document.querySelector(".hero-img");
 
-window.addEventListener('DOMContentLoaded', () => {
-  showHeroImage();
+window.addEventListener("DOMContentLoaded", () => {
+  setTimeout(() => {
+    heroImg.classList.add("img-translate");
+  }, 300);
 });
-
-function showHeroImage() {
-  heroImg.classList.add('img-translate');
-}
 
 function toggleElementClass(element, className) {
   element.classList.toggle(className);
 }
 
-function toggleBurgerAnimation() {
-  burgerIcons.forEach((icon) => {
-    toggleElementClass(icon, 'animate');
-  });
-}
+burgerMenuButton.addEventListener("click", () => {
+  hamburgerIcon.classList.toggle("animate");
 
-function toggleSidebar() {
-  toggleElementClass(sideBar, 'show');
-}
-
-function toggleMenuOpacity() {
-  toggleElementClass(menu, 'menu-opacity');
-}
-
-function toggleNavbarColor() {
-  toggleElementClass(navBar, 'navbar-color');
-}
-
-burgerMenuButton.addEventListener('click', () => {
-  toggleBurgerAnimation();
-  toggleSidebar();
-  toggleMenuOpacity();
-  toggleNavbarColor();
+  sideBar.classList.toggle("show");
+  menu.classList.toggle("menu-opacity");
 });
